ffc83f4586e8c078c442e446b628fe7ca29ce9fd
Civa <44102586+civa@users.noreply.github.com>
2021-03-14 09:02:57 +0100

    hawk

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,137 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://bsc.farswap.io",
+      },
+      {
+        label: "Liquidity",
+        href: "https://bsc.farswap.io/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "http://fi.farswap.io/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://farswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://farswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://farswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://farswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Voting",
+        href: "https://voting.fi.farswap.io",
+      },
+      {
+        label: "Github",
+        href: "https://github.com/farswap",
+      },
+      {
+        label: "Docs",
+        href: "https://docs.fi.farswap.io",
+      },
+      {
+        label: "Blog",
+        href: "https://pancakeswap.medium.com",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "Global",
+        href: "https://t.me/farswap_global",
+      }
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/farswap_defi",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
